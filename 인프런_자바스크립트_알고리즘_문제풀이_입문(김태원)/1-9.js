215b7d40cda86846bcc9f8ba38ff8292d10b6d4d
Seoin <68271159+tjdls111@users.noreply.github.com>
2023-12-07 11:43:16 +0900

    문제 번호 수정

--- 인프런_자바스크립트_알고리즘_문제풀이_입문(김태원)/1-9.js
+++ 인프런_자바스크립트_알고리즘_문제풀이_입문(김태원)/1-9.js
@@ -1,47 +1,15 @@
-// 9개 숫자 중 7개 숫자의 합이 특정 값(100)이 되어야 함. (여러 정답 가능)
-// 조합 문제? 9C7 했을 때 합이 100 
+// '대문자로 입력된 단어가 입력되면' : 단어 전체가 대문자인지 판단 필요 -> 아스키 코드로 판단해야 한다고 생각함.
+// 그런데 조건이 대문자로 입력된 단어가 들어온다는 뜻이었음
+const sol = (voca)=>{
+	// const isAllCapital = ()=>{
+	// 	for ( let i =0;i<=voca.length;i++){
+	// 		if (07 <= v.charCodeAt(i)  && v.charCodeAt(i) <=122) continue
+	// 		return false
+	// 	}
+	// 	return true
+	// 	}	
 
-// 9개 중 2개를 뽑아서 (전체 값의 합) - (그 두값의 합)이 100인지 확인해도 됨
-
-const getCombinations = (numbers, selectNumber)=> {
-	const result = []
-
-	// 1개만 고를 때
-	if (selectNumber === 1) return numbers.map(el=>[el])
-	
-	// python의 for i in array 와 비슷한 것 같다! 
-	numbers.forEach((fixed, index, origin)=>{
-
-		// 고정된 위치를 제외하고 바뀔 수 있는 부분
-		const rest = origin.slice(index+1)
-		
-		// 고정된 걸 빼고, 나머지 조합 구하기
-		const vals = getCombinations(rest, selectNumber-1)
-		
-		// 고정된 것과 나머지 조합 합치기
-		const attached = vals.map(el=>[fixed, ...el])
-		
-		result.push(...attached)
-		
-	})
-	return result
-}
-const sol = (heights)=>{
-	const height_list = heights.split(' ').map(i=>Number(i))
-	// console.log('height_list', height_list)
-	const combinations = getCombinations(height_list, 7)
-
-	// forEach에서는 break 없음. 중간에 멈추기 위해서 try, catch 이용할 수 있지만 코드 가독성 떨어짐
-	// some 이용 가능(조건 true되면 순회 멈춤)
-	// 참고 : https://blog.outsider.ne.kr/847
-	combinations.some(seven_dwarf_heights=>{
-		const seven_dwarf_heights_sum = seven_dwarf_heights.reduce((accumulator, currentValue)=>accumulator+currentValue,0)
-		if (seven_dwarf_heights_sum === 100) {
-			console.log(...seven_dwarf_heights)
-			return true
-		}
-		})
-	
-	
-}
-sol('20 7 23 19 10 15 25 8 13') //20 7 23 19 10 8 13
+	// return isAllCapital ? voca.replace(/A/g, '#') : voca
+	return voca.replace(/A/g, '#') 
+}	
+console.log(sol('BANANA')) //B#N#N#
